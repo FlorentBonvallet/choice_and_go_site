0b83b6dc286775c0d5bb647510b199fd5ca608ac
FlorentBonvallet <florentbonvallet@gmail.com>
2025-11-17 16:43:18 +0100

    Ajout de donnée au script
    Ajout de donnée de base au script de bd

--- sae.sql
+++ sae.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : lun. 17 nov. 2025 à 15:34
+-- Généré le : lun. 17 nov. 2025 à 15:40
 -- Version du serveur : 8.4.3
 -- Version de PHP : 8.3.16
 
@@ -35,6 +35,16 @@ CREATE TABLE `reservations` (
   `date_reservation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Déchargement des données de la table `reservations`
+--
+
+INSERT INTO `reservations` (`reservation_id`, `trajet_id`, `passager_id`, `statut_reservation`, `date_reservation`) VALUES
+(1, 1, 2, 'confirmée', '2025-11-17 16:39:35'),
+(2, 1, 4, 'confirmée', '2025-11-17 16:39:35'),
+(3, 2, 3, 'en_attente', '2025-11-17 16:39:35'),
+(4, 3, 5, 'confirmée', '2025-11-17 16:39:35');
+
 -- --------------------------------------------------------
 
 --
@@ -58,6 +68,16 @@ CREATE TABLE `trajets` (
   `date_creation_trajet` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Déchargement des données de la table `trajets`
+--
+
+INSERT INTO `trajets` (`trajet_id`, `conducteur_id`, `vehicule_id`, `lieu_depart`, `lieu_arrivee`, `date_heure_depart`, `places_disponibles`, `prix_par_place`, `statut_trajet`, `depart_latitude`, `depart_longitude`, `arrivee_latitude`, `arrivee_longitude`, `date_creation_trajet`) VALUES
+(1, 1, 1, 'IUT Lyon', 'Grenoble Campus', '2025-03-01 08:00:00', 3, 8.00, 'ouvert', NULL, NULL, NULL, NULL, '2025-11-17 16:39:25'),
+(2, 2, 2, 'IUT Lyon', 'Université Saint-Étienne', '2025-03-02 09:30:00', 2, 5.00, 'ouvert', NULL, NULL, NULL, NULL, '2025-11-17 16:39:25'),
+(3, 3, 3, 'IUT Lille', 'Valenciennes', '2025-03-04 07:15:00', 4, 6.50, 'ouvert', NULL, NULL, NULL, NULL, '2025-11-17 16:39:25'),
+(4, 1, 1, 'IUT Toulouse', 'Montpellier', '2025-03-05 10:45:00', 2, 9.00, 'ouvert', NULL, NULL, NULL, NULL, '2025-11-17 16:39:25');
+
 -- --------------------------------------------------------
 
 --
@@ -74,6 +94,17 @@ CREATE TABLE `utilisateurs` (
   `date_inscription` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`utilisateur_id`, `prenom`, `nom`, `email`, `mot_de_passe`, `telephone`, `date_inscription`) VALUES
+(1, 'Lucas', 'Martin', 'lucas.martin@etu.com', '$2y$10$abcdefghijklmnopqrstuv', '0601020304', '2025-11-17 16:38:46'),
+(2, 'Sarah', 'Dupont', 'sarah.dupont@etu.com', '$2y$10$qrstuvwxyzabcdefghijk', '0605060708', '2025-11-17 16:38:46'),
+(3, 'Hugo', 'Leroy', 'hugo.leroy@etu.com', '$2y$10$lmnopqrstuvabcdefghi', '0609080706', '2025-11-17 16:38:46'),
+(4, 'Emma', 'Roux', 'emma.roux@etu.com', '$2y$10$abcdefabcdefabcdefab', '0611223344', '2025-11-17 16:38:46'),
+(5, 'Maxime', 'Petit', 'maxime.petit@etu.com', '$2y$10$12345123451234512345', '0699887766', '2025-11-17 16:38:46');
+
 -- --------------------------------------------------------
 
 --
@@ -88,6 +119,15 @@ CREATE TABLE `vehicules` (
   `couleur` varchar(30) DEFAULT NULL,
   `immatriculation` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `vehicules`
+--
+
+INSERT INTO `vehicules` (`vehicule_id`, `utilisateur_id`, `marque`, `modele`, `couleur`, `immatriculation`) VALUES
+(1, 1, 'Peugeot', '208', 'Bleu', 'AA-123-AA'),
+(2, 2, 'Renault', 'Clio', 'Rouge', 'BB-456-BB'),
+(3, 3, 'Citroen', 'C3', 'Noir', 'CC-789-CC');
 
 --
 -- Index pour les tables déchargées
@@ -132,25 +172,25 @@ ALTER TABLE `vehicules`
 -- AUTO_INCREMENT pour la table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `reservation_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `reservation_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `trajets`
 --
 ALTER TABLE `trajets`
-  MODIFY `trajet_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `trajet_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `utilisateur_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `utilisateur_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `vehicules`
 --
 ALTER TABLE `vehicules`
-  MODIFY `vehicule_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `vehicule_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Contraintes pour les tables déchargées
